a71c3294d8884fa32cf996faf7e0a0019581b2b8
marvan9712 <marvan.ekanayake9@gmail.com>
2025-05-09 12:55:12 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Adjust if the main component is named differently
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
